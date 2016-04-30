949baed2d0a48ef3afb3b9ca0a778358378aae73
Dakota Whelchel <dwhel001@ucr.edu>
2016-04-29 17:48:06 -0700

    Updated Object class.

--- ObjectBase.h
+++ ObjectBase.h
@@ -1,9 +1,9 @@
-#ifndef OBJ_BASE_CLASS
-#define OBJ_BASE_CLASS
+#ifndef _OBJ_H_
+#define _OBJ_H_
 
-
-class Object {
-    private:
+class Obj {
+    
+    protected:
     int xLocation;
     int yLocation;
     int height;
@@ -12,11 +12,19 @@ class Object {
     char visual;
     
     public:
-    Object() : xLocation(0), yLocation(0), height(0), width(0), speed(0), visual('0') { }
-    Object(int x, int y, int h, int w, double s, char v) : xLocation(x), yLocation(y), height(h), width(w), speed(s), visual(v) { }
+    Obj() : xLocation(0), yLocation(0), height(0), width(0), speed(0), visual('0') { }
+    Obj(int x, int y, int h, int w, double s, char v) : xLocation(x), yLocation(y), height(h), width(w), speed(s), visual(v) { }
     
     //DRAW OBJECT
     virtual const void drawObject() const = 0;
+    
+    //ACCESSORS
+    int getX() {return xLocation;}
+    int getY() {return yLocation;}
+    int getHeight() {return height;}
+    int getWidth() {return width;}
+    double getSpeed() {return speed;}
+    char getVisual() {return visual;}
     
     //MUTATORS
     const void newXpos(int x) { xLocation = x; } 
